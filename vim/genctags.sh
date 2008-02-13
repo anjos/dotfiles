d35a8d2dc0be4b8cb8733a37de64c637d6ab830a
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2008-02-13 13:02:49 +0000

    Add ctags support to vim
    git-svn-id: file:///home/andreanjos/svn/andre_dotfiles@42 b2e9302d-b4d2-0310-80c4-8586bce995ec

---
+++ vim/genctags.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# Created by Andre Anjos <andre.dos.anjos@cern.ch>
+# Qua 13 Fev 2008 13:47:51 CET
+
+# Generates 'ctags' for a given project hierarchy
+
+# A few variables you can configure
+DEST=${HOME}/.vim/tags
+OPT="--recurse --verbose=yes --totals=yes --exclude=@ctags-exclude.txt"
+
+if [ $# != 2 ]; then
+  echo "usage: $0 <project-name> <base-directory>+"
+  exit 1
+fi
+
+[ ! -d ${DEST} ] && mkdir -pv ${DEST}
+ctags ${OPT} -f ${DEST}/$1.tags $2
+exit $?
