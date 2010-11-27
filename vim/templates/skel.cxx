1d111e03fe3d912f1a4ea474d7e35062c86cd9cb
André Anjos <andre.anjos@idiap.ch>
2010-11-27 07:30:47 +0100

    Remove @file doxygen input on C/C++; Use @DATE@ everywhere; Support for scons and waf

--- vim/templates/skel.cxx
+++ vim/templates/skel.cxx
@@ -1,6 +1,6 @@
 /**
- * @file @FILE_EXT@
  * @author <a href="mailto:andre.dos.anjos@gmail.com">Andre Anjos</a> 
+ * @date @DATE@ 
  *
  * @brief  
  */
--- vim/templates/skel.php
+++ vim/templates/skel.php
@@ -1,4 +1,3 @@
 #!/usr/bin/env php
-
-# @file @FILE_EXT@ 
 # @author <a href="mailto:andre.dos.anjos@gmail.com">Andre Anjos</a> 
+# @date @DATE@ 
