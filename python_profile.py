51b59b327cc28cf27ede1f497590e9d2acbd63cc
Andre Anjos <andre.anjos@idiap.ch>
2013-08-07 13:19:41 +0200

    Python3 compatibility

--- python_profile.py
+++ python_profile.py
@@ -1,5 +1,5 @@
 # Created by Andre Anjos <andre.dos.anjos@cern.ch>
-# Qui 26 Jul 2007 11:04:58 CEST 
+# Qui 26 Jul 2007 11:04:58 CEST
 
 # This my user initilization file. To get it activated
 # whenever you start python, just make sure the environment
@@ -13,7 +13,7 @@ try:
     readline.read_history_file(histfile)
   atexit.register(readline.write_history_file, histfile)
 except ImportError:
-  print "Module readline not available."
+  print("Module readline not available.")
 else:
   import rlcompleter
   readline.parse_and_bind("tab: complete")
