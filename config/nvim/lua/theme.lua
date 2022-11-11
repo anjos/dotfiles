a05931e4c7af97d22d93f004c966f27064bef361
Andre Anjos <andre.dos.anjos@gmail.com>
2022-11-11 18:08:21 +0100

    [nvim] Allow more flexibility with the theme to use

--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -5,8 +5,8 @@ vim.opt.background = "dark"
 -- Theme configuration
 -- local theme_to_use = 'gruvbox'
 -- local theme_to_use = 'tokyonight'
--- local theme_to_use = 'tokyonight-night'
-local theme_to_use = 'nordfox'
+-- local theme_to_use = 'nordfox'
+local theme_to_use = 'tokyonight-night'
 vim.cmd('colorscheme ' .. theme_to_use)
 
 -- Lualine configuration
@@ -26,8 +26,10 @@ if theme_to_use:sub(-#"fox") == "fox" then
     local palette = require('nightfox.palette').load(theme_to_use)
     local Color = require("nightfox.lib.color")
     local faded0 = Color.from_hex(palette.bg1):brighten(-10):to_css()
-    vim.cmd('highlight SignColumn guibg=' .. faded0)
     local faded1 = Color.from_hex(palette.bg1):brighten(-5):to_css()
+
+    -- Applies fixes
+    vim.cmd('highlight SignColumn guibg=' .. faded0)
     vim.cmd('highlight LineNr guibg=' .. faded1)
 
     -- Reset CoC sign column symbols to use the right pallete colors for each of
@@ -42,18 +44,38 @@ if theme_to_use:sub(-#"fox") == "fox" then
     vim.cmd('highlight CocHintSign guibg=' .. faded0 ..
             ' guifg=' .. spec.diag.hint)
 
-    -- Indent guides configuration
-    vim.g.indent_guides_guide_size = 1
-
-    -- Lets me manage the gutter color, always show the bar
-    vim.opt.signcolumn = 'yes'
-    vim.g.gitgutter_grep = ''
-    vim.g.gitgutter_set_sign_backgrounds = 1
-
     -- show trailing whitespaces, handle it better
     vim.cmd('highlight ExtraWhitespace guibg=' .. spec.diag.warn)
 
 elseif theme_to_use:sub(1, #"tokyonight") == "tokyonight" then
 
     -- Sets of tokyonight theme details
+    local colors = require('tokyonight.colors').setup()
+    local util = require('tokyonight.util')
+
+    -- Defines some of the colors to use
+    local faded0 = util.darken(colors.bg, 0.5, '#000000')
+    local faded1 = util.darken(colors.bg, 0.5, faded0)
+
+    -- Applies fixes
+    vim.cmd('highlight SignColumn guibg=' .. faded0)
+    vim.cmd('highlight LineNr guibg=' .. faded1)
+
+    vim.cmd('highlight CocErrorSign guibg=' .. faded0 ..
+            ' guifg=' .. colors.red)
+    vim.cmd('highlight CocWarningSign guibg=' .. faded0 ..
+            ' guifg=' .. colors.yellow)
+    vim.cmd('highlight CocInfoSign guibg=' .. faded0 ..
+            ' guifg=' .. colors.green)
+    vim.cmd('highlight CocHintcolors guibg=' .. faded0 ..
+            ' guifg=' .. colors.magenta)
+
 end
+
+-- Indent guides configuration
+vim.g.indent_guides_guide_size = 1
+
+-- Lets me manage the gutter color, always show the bar
+vim.opt.signcolumn = 'yes'
+vim.g.gitgutter_grep = ''
+vim.g.gitgutter_set_sign_backgrounds = 1
