e0979ff82a9ff65b67855fe05863377c3e4061c2
Andre Anjos <andre.dos.anjos@gmail.com>
2023-05-26 17:29:56 +0200

    [neovim] Revamp configuration with hints from kickstart.nvim

--- config/nvim/lua/behaviour.lua
+++ config/nvim/lua/behaviour.lua
@@ -1,3 +1,5 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
 vim.opt.mouse = ''        -- Mouse work fine against terminal neovim
 vim.opt.title = true      -- set terminal title
 vim.opt.autoread = true   -- re-read files if they change on disk
@@ -15,6 +17,12 @@ vim.opt.textwidth = 79    -- wrap automatically in 80 columns
 vim.opt.showmatch = true  -- to show matching pairs of brackets or braces
 vim.opt.modeline = true   -- let local buffers modify variables
 vim.opt.backspace = {'indent','eol','start'}  -- fixes backspace
+vim.opt.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.o.ignorecase = true   -- Case insensitive searching UNLESS /C or capital in search
+vim.o.smartcase = true
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
 
 -- HTML editing does not need line breaking...
 vim.api.nvim_create_autocmd("FileType",
@@ -51,9 +59,33 @@ vim.opt.wildignore = {
 	'__pycache__/',
 }
 
--- change the leader key from "\" to ";" ("," is also popular)
-vim.g.mapleader = ';'
-vim.g.maplocalleader = ';'
+-- If asked to display special characters (whitespaces), then use these symbols
+vim.opt.listchars='eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣'
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+require('telescope').load_extension('fzf')
 
 -- Temporarily disables highlighting with <SPACE>
 vim.keymap.set('n', '<SPACE>', '<SPACE>:noh<CR>')
@@ -61,15 +93,25 @@ vim.keymap.set('n', '<SPACE>', '<SPACE>:noh<CR>')
 -- Make Y yank the whole line
 vim.keymap.set('n', 'Y', 'yy')
 
--- Shortcut to edit THIS configuration file: (e)dit (c)onfiguration
-vim.keymap.set('n', '<leader>ec', ':e $MYVIMRC<CR>', {noremap = true, silent = true})
-
--- Shortcut to source (reload) THIS configuration file after editing it:
-vim.keymap.set('n', '<leader>sc', ':source $MYVIMRC<CR>', {noremap = true, silent = true})
-
 -- use ;; for escape (iPad keyboard)
 -- http://vim.wikia.com/wiki/Avoid_the_escape_key
 vim.keymap.set('i', ';;', '<Esc>', {noremap = true})
 
--- If asked to display special characters (whitespaces), then use these symbols
-vim.opt.listchars='eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣'
+-- See `:help telescope.builtin`
+local wk = require('which-key')
+wk.register({
+  ["<leader>ec"] = {"<cmd>edit $MYVIMRC<cr>", "[c] Edit neovim's init.lua", noremap = true, silent = true},
+  ["<leader>sc"] = {"<cmd>source $MYVIMRC<cr>", "[c] Reload neovim's init.lua", noremap = true, silent = true},
+  ["<leader>?"] = { "<cmd>Telescope oldfiles<cr>", "[?] Open recent file" },
+  ["<leader><space>"] = { "<cmd>Telescope buffers<cr>", "[<space>] Open recent buffer" },
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  ["<leader>/"] = { function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, }) end, '[/] Fuzzily search in current buffer' },
+  ["<leader>gf"] =  { "<cmd>Telescope git_files<cr>", "Search [g]it [f]iles" },
+  ["<leader>s"] =  {
+    f = { "<cmd>Telescope find_files<cr>", "[S]earch [f]iles" },
+    h = { "<cmd>Telescope help_tags<cr>", "[S]earch [h]elp" },
+    g = { "<cmd>Telescope live_grep<cr>", "[S]earch with [g]rep" },
+    w = { "<cmd>Telescope grep_string<cr>", "[S]earch current [w]ord with grep" },
+    d = { "<cmd>Telescope diagnostics<cr>", "[S]earch [d]iagnostics" },
+  },
+})
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -13,32 +13,70 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup({function(use)
-    use 'wbthomason/packer.nvim'  -- auto-managed packed
+    use 'wbthomason/packer.nvim'  -- auto-managed packer
 
-    -- Other plugins from here...
+    -- Use G<git-cmd> to check git command outputs on neovim
     use 'tpope/vim-fugitive'
-    use 'airblade/vim-gitgutter'
+
+    -- Figures out white-space and tab-stops cleverly
+    use 'tpope/vim-sleuth'
+
+    -- Functions for doing case-persistant substitutions
     use 'vim-scripts/keepcase.vim'
+
+    -- Snippet support
     use 'honza/vim-snippets'
+
+    -- Powerline support in neovim
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    -- Themes
     use 'EdenEast/nightfox.nvim'    -- nightfox themes
     use 'ellisonleao/gruvbox.nvim'  -- gruvbox theme
     use {'folke/tokyonight.nvim', branch = 'main'}  -- tokyonight theme
-    use 'ntpeters/vim-better-whitespace'
-    use 'lervag/vimtex'
-    use {'psf/black', tag='stable'}
 
+    -- Displays a popup with possible key bindings of the command you started
+    -- typing
     use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end
+        'folke/which-key.nvim',
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+            }
+        end
     }
 
-    use {
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    -- Adds gutter signs for git changes
+    use 'airblade/vim-gitgutter'
+
+    -- Cause all trailing whitespace characters to be highlighted
+    use 'ntpeters/vim-better-whitespace'
+
+    -- "gc" to comment visual regions/lines
+    use 'numToStr/Comment.nvim'
+
+    -- Helps typesetting LaTeX
+    use 'lervag/vimtex'
+
+    -- Auto-indent/re-indent Python files
+    use {'psf/black', tag='stable'}
+
+    -- Overlay window system for various activities: searching files, buffers,
+    -- completions, etc.
+    use {'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+    -- Code parser for neovim allows code highlighting, edition, and navigation
+    use {'nvim-treesitter/nvim-treesitter',
+        requires = { 'nvim-treesitter/nvim-treesitter-textobjects', opt = true },
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end
     }
 
     use {'neoclide/coc.nvim', branch='release'}
