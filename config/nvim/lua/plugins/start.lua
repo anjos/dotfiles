c352eee1b1b56c814e49560b619b891b63ff33ee
Andre Anjos <andre.dos.anjos@gmail.com>
2023-06-07 11:50:42 +0200

    [neovim] Prepare start page

--- config/nvim/lua/plugins/start.lua
+++ config/nvim/lua/plugins/start.lua
@@ -1,3 +1,137 @@
+--- Trims whitespaces from the begin and end of a string
+-- @str The string to be trimmed
+local trim = function(str)
+    return string.gsub(str, '^%s*(.-)[\r\n%s]*$', '%1')
+end
+
+--- Returns a string containing the currently defined conda environment
+-- @symbol If you set symbol, then it prefixes the string with a symbol
+local conda_env = function(symbol)
+    local value = trim(os.getenv('CONDA_DEFAULT_ENV') or '[not set]')
+    return string.format('%s%s', symbol or '', value)
+end
+
+--- Returns a string containing the current python interpreter version
+-- @symbol If you set symbol, then it prefixes the string with a symbol
+local python_version = function(symbol)
+    local value = trim(vim.fn.system({
+        'python',
+        '-c',
+        'print(".".join(map(str, __import__("sys").version_info[:3])))',
+    }))
+    value = value or '[not set]'
+    return string.format('%s%s', symbol or '', value)
+end
+
+--- Returns a string containing the name of the base directory
+-- of the current git repository (if there is one), or an empty string
+-- if we are not in a git repository
+-- @symbol If you set the symbol, then it prefixes with that
+local git_basedir = function(symbol)
+    local value = vim.fs.basename(
+        trim(vim.fn.system('git rev-parse --show-toplevel 2>/dev/null'))
+    )
+    if value ~= '' then
+        return string.format('%s%s', symbol or '', value)
+    end
+    return nil
+end
+
+--- Returns a string containing the name of the base directory
+-- of the current git repository (if there is one), or an empty string
+-- if we are not in a git repository
+-- @symbol If you set the symbol, then it prefixes with that
+local basedir = function(symbol)
+    local value = vim.fn.getcwd()
+    if value == os.getenv('HOME') then
+        value = '~'
+    else
+        value = vim.fs.basename(value)
+    end
+    return string.format('%s%s', symbol or '', value)
+end
+
+--- Returns a string containing the name of the current git branch (if there is
+-- one), or an empty string
+-- @symbol If you set the symbol, then it prefixes with that
+local git_branch = function(symbol)
+    local value = trim(vim.fn.system('git branch --show-current 2>/dev/null'))
+    if value ~= '' then
+        return string.format('%s%s', symbol or '', value)
+    end
+    return ''
+end
+
+--- Merges various strings with a separator, but only if they are not empty, in
+-- which case they are discarded from the output
+local merge_non_empty = function(strs, merge_symbol)
+    local t = {}
+    for _, value in ipairs(strs) do
+        if value ~= '' then
+            table.insert(t, value)
+        end
+    end
+    return table.concat(t, merge_symbol)
+end
+
+local neovim_logo1 = {
+    '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
+    '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
+    '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
+    '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
+    '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
+    '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
+}
+
+local neovim_logo2 = {
+    ' ██████   █████                   █████   █████  ███                 ',
+    '░░██████ ░░███                   ░░███   ░░███  ░░░                  ',
+    ' ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████  ',
+    ' ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███ ',
+    ' ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███ ',
+    ' ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███ ',
+    ' █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████',
+    '░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░ ',
+}
+
+local mario_head = {
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢉⠉⠙⢿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠎⢷⣄⠀⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⣠⣴⣶⣶⣤⣤⣄⣀⠀⠁⠠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⠙⠿⢿⣿⣿⣿⣿⣿⣿⣷⣦⣌⠙⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀ ',
+    '  ⠀⠀⠀⠀⠀⠀⠈⠉⢻⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠉⠙⢿⣿⣿⣿⢠⣤⣤⣄ ',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢹⣿⣿⣿⣿⣿⣿⣧',
+    '  ⢠⣾⣿⣿⣿⣶⣤⣀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠻⠿⠿⣿⣿⡿⠃',
+    '  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠉⢹⣿⣟⠁⠀⠀⠀⢀⣴⣾⣿⣶⡍⠀⠀',
+    '  ⠘⠿⣿⣿⣿⣿⡿⠟⠋⠘⠛⠋⠁⠀⠀⢠⣿⡏⠀⠀⢀⣰⣿⣿⣿⣿⣿⡗⠀⠀',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀',
+    '  ⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⣠⣤⣴⣿⣿⣿⣿⣿⣿⣿⠟⠉⠙⠋⠉⠀⠀⠀⠀',
+    '  ⠀⠀⠀⠀⠀⠀⠙⠛⠲⢶⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+}
+
+local make_header = function()
+    local header = neovim_logo1
+    local info = {
+        '',
+        os.date('%A, %d %B %Y - %H:%M:%S, %Z'),
+        '',
+        merge_non_empty({
+            git_basedir('  ') or basedir('  '),
+            git_branch(' '),
+            conda_env('🅒  '),
+            python_version('🐍 '),
+        }, ' | '),
+        '',
+    }
+    for _,v in ipairs(info) do
+        table.insert(header, v)
+    end
+    return header
+end
+
 return {
 
     {
@@ -6,27 +140,9 @@ return {
         event = 'VimEnter',
         config = function()
             require('dashboard').setup({
+                theme = 'hyper',
                 config = {
-                    header = {
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢉⠉⠙⢿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠎⢷⣄⠀⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⣠⣴⣶⣶⣤⣤⣄⣀⠀⠁⠠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⠙⠿⢿⣿⣿⣿⣿⣿⣿⣷⣦⣌⠙⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀ ',
-                        '  ⠀⠀⠀⠀⠀⠀⠈⠉⢻⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠉⠙⢿⣿⣿⣿⢠⣤⣤⣄ ',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢹⣿⣿⣿⣿⣿⣿⣧',
-                        '  ⢠⣾⣿⣿⣿⣶⣤⣀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠻⠿⠿⣿⣿⡿⠃',
-                        '  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠉⢹⣿⣟⠁⠀⠀⠀⢀⣴⣾⣿⣶⡍⠀⠀',
-                        '  ⠘⠿⣿⣿⣿⣿⡿⠟⠋⠘⠛⠋⠁⠀⠀⢠⣿⡏⠀⠀⢀⣰⣿⣿⣿⣿⣿⡗⠀⠀',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀',
-                        '  ⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⣠⣤⣴⣿⣿⣿⣿⣿⣿⣿⠟⠉⠙⠋⠉⠀⠀⠀⠀',
-                        '  ⠀⠀⠀⠀⠀⠀⠙⠛⠲⢶⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-                        '  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-                        '                                ',
-                        os.date('%A, %d %B %Y - %H:%M:%S, %Z'),
-                        '                                ',
-                    },
+                    header = make_header(),
                     shortcut = {
                         -- action can be a function type
                         {
@@ -36,27 +152,35 @@ return {
                             action = function()
                                 require('telescope.builtin').git_files({
                                     layout_strategy = 'flex',
-                                    layout_config = { width = 0.9, height = 0.9 },
-                                }) end,
+                                    layout_config = {
+                                        width = 0.9,
+                                        height = 0.9,
+                                    },
+                                })
+                            end,
                         },
                         {
-                            desc = '| Open files ',
+                            desc = '|  Open files ',
                             key = 'f',
                             group = 'DiagnosticWarn',
                             action = function()
                                 require('telescope.builtin').find_files({
                                     layout_strategy = 'flex',
-                                    layout_config = { width = 0.9, height = 0.9 },
-                                }) end,
+                                    layout_config = {
+                                        width = 0.9,
+                                        height = 0.9,
+                                    },
+                                })
+                            end,
                         },
                         {
-                            desc = '| Grep ',
+                            desc = '|  Grep ',
                             key = 'x',
                             group = 'DiagnosticWarn',
                             action = 'Telescope live_grep',
                         },
                         {
-                            desc = '| Search help ',
+                            desc = '|  Search help ',
                             key = 'h',
                             group = 'DiagnosticWarn',
                             action = 'Telescope help_tags',
@@ -69,10 +193,10 @@ return {
                     },
                     footer = {
                         ' ',
-                        '💡 André Anjos (https://anjos.ai)',
-                        ' ',
-                        ' ' .. os.date("%Y"),
-                    }
+                        '💡 André Anjos (https://anjos.ai) '
+                            .. ' '
+                            .. os.date('%Y'),
+                    },
                 },
             })
         end,
