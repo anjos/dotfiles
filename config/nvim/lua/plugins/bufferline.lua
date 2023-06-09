fc84629cfa7ebc61839630da612ed00212091eb1
Andre Anjos <andre.dos.anjos@gmail.com>
2023-06-09 09:03:22 +0200

    [neovim] Use barbar plugin instead of bufferline

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -12,41 +12,56 @@ local diagnostics_indicator = function(count, level, diagnostics_dict, context)
 end
 
 return {
-    {
-        'akinsho/bufferline.nvim',
-        version = '*',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            vim.opt.termguicolors = true
-            vim.opt.mousemoveevent = true
-            vim.opt.mouse = 'a'
-            require('bufferline').setup({
-                options = {
-                    diagnostics = 'nvim_lsp',
-                    diagnostics_indicator = diagnostics_indicator,
-                    hover = {
-                        enabled = true,
-                        delay = 200,
-                        reveal = { 'close' },
-                    },
-                    numbers = 'ordinal',
-                },
-            })
 
-            -- keymaps to easily access buffers
-            for j = 1, 9, 1 do
-                vim.keymap.set(
-                    { 'n', 'v' },
-                    string.format('<leader>%d', j),
-                    function()
-                        require('bufferline').go_to(j, true)
-                    end,
-                    { desc = string.format('Go to buffer #%d', j) }
-                )
-            end
-            vim.keymap.set({ 'n', 'v' }, '<leader>$', function()
-                require('bufferline').go_to(-1, true)
-            end, { desc = 'Go to the last buffer' })
+    -- {
+    --     'akinsho/bufferline.nvim',
+    --     version = '*',
+    --     dependencies = 'nvim-tree/nvim-web-devicons',
+    --     config = function()
+    --         vim.opt.termguicolors = true
+    --         vim.opt.mousemoveevent = true
+    --         vim.opt.mouse = 'a'
+    --         require('bufferline').setup({
+    --             options = {
+    --                 diagnostics = 'nvim_lsp',
+    --                 diagnostics_indicator = diagnostics_indicator,
+    --                 hover = {
+    --                     enabled = true,
+    --                     delay = 200,
+    --                     reveal = { 'close' },
+    --                 },
+    --                 numbers = 'ordinal',
+    --             },
+    --         })
+    --
+    --         -- keymaps to easily access buffers
+    --         for j = 1, 9, 1 do
+    --             vim.keymap.set(
+    --                 { 'n', 'v' },
+    --                 string.format('<leader>%d', j),
+    --                 function()
+    --                     require('bufferline').go_to(j, true)
+    --                 end,
+    --                 { desc = string.format('Go to buffer #%d', j) }
+    --             )
+    --         end
+    --         vim.keymap.set({ 'n', 'v' }, '<leader>$', function()
+    --             require('bufferline').go_to(-1, true)
+    --         end, { desc = 'Go to the last buffer' })
+    --     end,
+    -- },
+
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            -- 'lewis6991/gitsigns.nvim', -- configured on 'ui' plugins
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function()
+            vim.g.barbar_auto_setup = false
         end,
+        opts = {
+            auto_hide = true,
+        },
     },
 }
