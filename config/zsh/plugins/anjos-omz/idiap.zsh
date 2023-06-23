8611130df60fe24cf740e745527c0b56983ed672
Andre Anjos <andre.dos.anjos@gmail.com>
2023-06-23 11:48:54 +0200

    [zsh/anjos-omz/idiap] Add mount/unmount functions

--- config/zsh/plugins/anjos-omz/idiap.zsh
+++ config/zsh/plugins/anjos-omz/idiap.zsh
@@ -1,5 +1,5 @@
 # A function to start a new tunnel to my machine at Idiap
-function rdmac () {
+function idiap-remote-desktop-macos {
   if [[ $1 == "-h" ]]; then
     echo "Enables a remote desktop to an idiap mac ci:"
     echo "$ rdmac"
@@ -32,6 +32,61 @@ function rdmac () {
 }
 
 # This starts a VPN tunnel to Idiap
-function vpn() {
+function idiap-vpn {
     sudo openfortivpn sslvpn.idiap.ch:443 --username=aanjos --trusted-cert 36e9b7ab8fac6699da2f617a58b4fb32e140a62352bcde30569d6a5ea09d0b4e --pppd-use-peerdns=1
+}
+
+# Mounts important Idiap file systems to my mac
+# Uses SSHFS, that must be installed via `brew install sshfs`
+# SSHFS requires FUSE for macOS, that can be installed from
+# https://osxfuse.github.io
+function idiap-mount {
+    local host="idiap"  #shortcut on my ~/.ssh/config for my Idiap login
+
+    # Notice $mount/$dest should have the same length
+    local mount=()
+    local dest=()
+
+    mount+=("/idiap/temp/aanjos")
+    dest+=("$HOME/mnt/idiap/temp")
+
+    mount+=("/idiap/user/aanjos")
+    dest+=("$HOME/mnt/idiap/user")
+
+    mount+=("/idiap/home/aanjos")
+    dest+=("$HOME/mnt/idiap/home")
+
+    local mounted=$(mount)
+
+    for k in "${!mount[@]}"; do
+        if [ -z "${mounted##*${dest[$k]}*}" ]; then  #not mounted yet
+            echo "[anjos-idiap] ${host}:${mount[$k]} -> ${dest[$k]} (skip - already mounted)"
+        else
+            d=$(ssh ${host} "readlink ${mount[$k]}")  #figures out real name
+            echo "[anjos-idiap] ${host}:${mount[$k]} -> ${dest[$k]}"
+            sshfs "${host}:${d}" "${dest[$k]}"
+        fi
+    done
+}
+
+# Unmounts important Idiap file systems from my mac
+function idiap-unmount {
+
+    local dest=()
+
+    dest+=("$HOME/mnt/idiap/temp")
+    dest+=("$HOME/mnt/idiap/user")
+    dest+=("$HOME/mnt/idiap/home")
+
+    local mounted=$(mount)
+
+    for k in "${dest[@]}"; do
+        if [ -z "${mounted##*$k*}" ]; then
+            echo "umount $k"
+            umount $k
+        else
+            echo "$k is not mounted"
+        fi
+    done
+
 }
