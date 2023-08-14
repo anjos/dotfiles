c8b9ec28f8c7736704011b36602a03f7609bf10e
Andre Anjos <andre.dos.anjos@gmail.com>
2023-08-14 10:11:51 +0200

    [zsh] Better terminal setup

--- config/zsh/plugins/anjos-omz/terminal-setup.zsh
+++ config/zsh/plugins/anjos-omz/terminal-setup.zsh
@@ -1,13 +1,16 @@
 # Integration with iTerm2 and Kitty
-if [[ -n $ITERM_SESSION_ID && -r "${ZSH_CUSTOM}/iterm2_shell_integration.zsh" ]]; then
+if [[ -n "${ITERM_SESSION_ID}" && -r "${ZSH_CUSTOM}/iterm2_shell_integration.zsh" ]]; then
+    #echo "Enabling iTerm2 integration..."
     source "${ZSH_CUSTOM}/iterm2_shell_integration.zsh"
-elif [[ -n $KITTY_INSTALLATION_DIR ]]; then
+elif [[ -n "${KITTY_INSTALLATION_DIR}" ]]; then
+    #echo "Enabling Kitty integration..."
     export KITTY_SHELL_INTEGRATION="enabled"
     autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
     kitty-integration
     unfunction kitty-integration
 fi
 
+# Aliases specifically for Kitty
 if [[ "$TERM" == "xterm-kitty" ]]; then
     alias ssh="kitty +kitten ssh"
     function svim() {
