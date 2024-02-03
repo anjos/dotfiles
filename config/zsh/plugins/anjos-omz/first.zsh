9525b23672530fb317c88b5db9e27c7affde48d2
Andre Anjos <andre.dos.anjos@gmail.com>
2024-02-03 19:10:10 +0100

    Add snap store path

--- config/zsh/plugins/anjos-omz/first.zsh
+++ config/zsh/plugins/anjos-omz/first.zsh
@@ -3,3 +3,8 @@ if [ -x /usr/libexec/path_helper ]; then
   # Updates values at /etc/paths and /etc/manpaths
   eval `/usr/libexec/path_helper`
 fi
+
+if [ -d /snap/bin ]; then
+  # Snap binaries on Linux
+  export PATH=/snap/bin:$PATH
+fi
