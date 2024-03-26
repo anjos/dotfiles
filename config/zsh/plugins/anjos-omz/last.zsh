1a1f4077a3b5cf080570ad00c11bbd5a91db5cac
Andre Anjos <andre.dos.anjos@gmail.com>
2024-03-26 09:44:59 +0100

    [zsh/last] Deprecate personal bin directory

--- config/zsh/plugins/anjos-omz/last.zsh
+++ config/zsh/plugins/anjos-omz/last.zsh
@@ -1,8 +1,3 @@
-# Adds our own executables in front of the path
-if [ -d $HOME/bin ]; then
-  export PATH=$HOME/bin:$PATH;
-fi
-
 # Removes duplicates from PATH and MANPATH
 export PATH=$(awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}'<<<$PATH)
 export MANPATH=$(awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}'<<<$MANPATH)
